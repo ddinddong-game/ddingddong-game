a698e8cb283a438e179d4b69f29a418e1bb76e59
JH <vvaa277@gmail.com>
2023-05-16 17:53:35 +0900

    chore: 파이어베이스 설치 및 초기화
    - 파이어베이스 설치, sdk추가
    
    issue #4

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyCJH9MYUjeAfJ-i5cNcc5k3vTiJlTi2wJw',
+	authDomain: 'ddingddong-game.firebaseapp.com',
+	databaseURL: 'https://ddingddong-game-default-rtdb.asia-southeast1.firebasedatabase.app',
+	projectId: 'ddingddong-game',
+	storageBucket: 'ddingddong-game.appspot.com',
+	messagingSenderId: '360917976342',
+	appId: '1:360917976342:web:1863d5e56d8e1a9414fc37',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
