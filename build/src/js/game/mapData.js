fa0b5fb023186cf414c63a96bf375fe62f14f6c8
JH <vvaa277@gmail.com>
2023-05-18 22:53:54 +0900

    feat: 맵 데이터 설정
    - 지나갈 수 없는 스페이스 설정
    
    issue #25

---
+++ build/src/js/game/mapData.js
@@ -0,0 +1,20 @@
+export const mapData = {
+	minX: 1,
+	maxX: 14,
+	minY: 4,
+	maxY: 12,
+	blockedSpaces: {
+		'7x4': true,
+		'1x11': true,
+		'12x10': true,
+		'4x7': true,
+		'5x7': true,
+		'6x7': true,
+		'8x6': true,
+		'9x6': true,
+		'10x6': true,
+		'7x9': true,
+		'8x9': true,
+		'9x9': true,
+	},
+};
